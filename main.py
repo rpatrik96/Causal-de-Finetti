e25cf8d6013b53529a3c6f113315e5817f73f319
Patrik Reizinger <prmedia17@gmail.com>
2024-11-13 16:04:17 +0100

    remove matlab from requrements

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from multivariate_experiment import *
+# from multivariate_experiment import *
 from bivariate_experiment import *
 
 def main():
     """ Main function for bivariate and multivariate multi-environment causal discovery algorithms.
     """
-    _ = bivariate_multienv(scm_bivariate_continuous, "./experiments/results/", ['Causal-de-Finetti', 'FCI', 'GES', 'NOTEARS', 'DirectLinGAM', 'CD-NOD'],
+    _ = bivariate_multienv(scm_bivariate_continuous, "./experiments/results/", ['Causal-de-Finetti', 'FCI', 'GES', 'NOTEARS', 'DirectLinGAM'],
                            [100, 200, 300, 400, 500])
     #_ = multivariate_multienv(scm_multivariate_binary, "./experiments/results/", ['Causal-de-Finetti', 'PC', 'FCI', 'GES', 'NOTEARS'], [2000, 4000, 6000, 8000, 10000], num_var=3)
+
+    # run scm_bivariate_continuous foor testing the cause/mechanism variability
+    # data = scm_bivariate_continuous(num_env = 3, num_sample = 5, exchg_mode = 1)
+    # print(data['true_structure'])
 
 if __name__ == "__main__":
     main()
